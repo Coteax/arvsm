c60910cda1be83afba3ff85a8ea9b62bedbe1664
PGouv <info@pgouv.com>
2016-02-21 00:04:07 +0000

    Tidy up request.rb

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,16 +1,15 @@
 class Request < ActiveRecord::Base
   # Status and Absense Type Enums
-  enum status: { requested: 'status_requested',
-                 approved: 'status_approved',
-                 denied: 'status_denied' }
+  enum status:       { requested: 'status_requested',
+                       approved: 'status_approved',
+                       denied: 'status_denied' }
 
   enum absense_type: { full: 'absense_full',
                        sickness: 'absense_sickness',
                        partial: 'absense_partial' }
 
-
   # Non persisted virtual atrributes for partial absense functionality
-  attr_accessor  :time_starting, :time_ending
+  attr_accessor :partial_starting, :time_starting, :time_ending
 
   # Setup relations
   belongs_to :user
@@ -24,23 +23,21 @@ class Request < ActiveRecord::Base
   validates :description, length: { minimum: 20 }
 
   # Validate these attributes only for non partial absense types
-  with_options unless: :is_partial? do |np|
+  with_options unless: :partial? do |np|
     # Absense starting date should be after tommorow
-    np.validates_date :starting, :on => :create, :on_or_after => :today
+    np.validates_date :starting, on: :create, on_or_after: :today
     # Absense ending date should be after starting
-    np.validates_date :ending, :on => :create, :after => :starting
+    np.validates_date :ending, on: :create, after: :starting
 
     np.validates      :days_off,
-                       numericality: { only_integer: true,
-                                       greater_than_or_equal_to: 1 }
-
+                      numericality: { only_integer: true,
+                                      greater_than_or_equal_to: 1 }
   end
   # Validate these fields only for partial absense type
-  with_options if: :is_partial?,on: :create do |p|
-    p.validates_date :partial_starting, :on_or_after => :today
+  with_options if: :partial?, on: :create do |p|
+    p.validates_date :partial_starting, on_or_after: :today
     p.validates_time :time_starting
-    p.validates_time :time_ending, :after => :time_starting
-
+    p.validates_time :time_ending, after: :time_starting
   end
   # Initialize to Request status
   after_initialize :set_default_values
@@ -49,15 +46,4 @@ class Request < ActiveRecord::Base
   def set_default_values
     self.absense_type ||= Request.absense_types[:full]
   end
-
-  def partial_starting
-    @partial_starting
-  end
-  def partial_starting=(val)
-    @partial_starting=val
-  end
-  def is_partial?
-    partial?
-  end
-
 end
